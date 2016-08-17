c69f5376437c2ab1b5ff3e1ef51e5f438a1012eb
Georgy Shabunin <georgy.shabunin@gmail.com>
2016-08-17 09:39:06 +0300

    adds a login screen

---
+++ Login.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  Image,
+  ActivityIndicatorIOS,
+  View
+} from 'react-native';
+
+class Login extends React.Component
+{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      showProgress: false
+    };
+  }
+
+  onLoginPressed(){
+    console.log("attempting to login with username " + this.state.username);
+    this.setState({showProgress: true});
+
+
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Image style={styles.logo} source={require('./img/Octocat.png')} />
+        <Text style={styles.heading}>GitHub Browser</Text>
+
+        <TextInput
+          onChangeText={(text) => {this.setState({username: text})}}
+          style={styles.loginInput}
+          placeholder="GitHub Username"
+        />
+        <TextInput
+          onChangeText={(text) => {this.setState({password: text})}}
+          style={styles.loginInput}
+          placeholder="GitHub Password"
+          secureTextEntry={true}
+        />
+
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.onLoginPressed.bind(this)}
+        >
+          <Text style={styles.buttonText}>Log in</Text>
+        </TouchableHighlight>
+
+        <ActivityIndicatorIOS
+          animating={this.state.showProgress}
+          style={styles.loader}
+          size="large"
+        />
+      </View>
+      );
+  }
+}
+
+var styles=StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 10,
+    paddingTop: 40,
+    backgroundColor: '#F5FCFF',
+
+  },
+  logo: {
+    width: 66,
+    height: 55
+  },
+  heading: {
+    fontSize: 30,
+    margin: 10,
+    marginTop: 20
+  },
+  loginInput: {
+      height: 50,
+      marginTop: 10,
+      padding: 4,
+      fontSize: 18,
+      borderWidth: 1,
+      borderColor: '#48BBEC',
+      borderRadius: 0,
+      color: '#48BBEC'
+  },
+  button: {
+      height: 50,
+      backgroundColor: '#48BBEC',
+      borderColor: '#48BBEC',
+      alignSelf: 'stretch',
+      marginTop: 10,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 5
+  },
+  buttonText: {
+      color: '#fff',
+      fontSize: 24
+  },
+  loader: {
+    marginTop: 20
+  }
+});
+
+export default Login;
--- index.ios.js
+++ index.ios.js
@@ -4,49 +4,61 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import {
   AppRegistry,
-  StyleSheet,
   Text,
-  View
+  View,
+  ActivityIndicatorIOS,
+  StyleSheet
 } from 'react-native';
+import Login from './Login';
+// import AuthService from './AuthService';
 
-class GithubBrowser extends Component {
+class GithubBrowser extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      checkingAuth: false,
+      isLoggedIn: false
+    }
+  }
+
+  componentDidMount(){
+    // AuthService.getAuthInfo((err, authInfo) => {
+    //   this.setState({
+    //     checkingAuth: false,
+    //     isLoggedIn: authInfo != null
+    //   });
+    // })
+  }
+
+
   render() {
+    if(this.state.checkingAuth){
+      return (
+        <View style={styles.container}>
+          <ActivityIndicatorIOS
+            animating={true}
+            size="large"
+            style={styles.loader}
+          />
+        </View>
+        );
+    }
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to our React Native course!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Login />
     );
   }
 }
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
